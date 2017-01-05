349b0e54fc1c0e4bf6d90ab037b7cbd4aaabf20a
Zhang Guhua <iuyo5678@gmail.com>
2017-01-05 18:57:23 +0800

    first add file

---
+++ App.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Time-stamp: <2017-01-05 17:35:23 Thursday by wls81>
+
+from Tkinter import *
+import tkMessageBox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        tkMessageBox.showinfo('Message', 'Hello, %s' % name)
+
+def main():
+    app = Application()
+    # 设置窗口标题:
+    app.master.title('Hello World')
+    # 主消息循环:
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
